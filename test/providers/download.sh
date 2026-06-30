b2d0ce481e98e91d7436bf6142f89835bf5670cf
Nicolay Rusnachenko <rusnicolay@gmail.com>
2026-06-30 13:30:05 +0100

    Updated: unit-test refers to providers in arexplorer-demo

--- test/providers/download.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-curl -L -o spacy_383.py https://raw.githubusercontent.com/nicolay-r/nlp-thirdgate/refs/heads/master/ner/spacy_383.py 
-curl -L -o replicate_104.py https://raw.githubusercontent.com/nicolay-r/nlp-thirdgate/refs/heads/master/llm/replicate_104.py
